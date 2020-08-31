3c0c87eaaabbb2e86631b68f842ba811df67dc1b
carina092 <carina@carinascheiberlich.nl>
2020-08-31 16:56:48 +0200

    Added reset function inside the animation section. Added (non functional) close button

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+// Typewriter
+var animationStarted = false;
+
+var flowInput = document.getElementById('flow-input');
+
+var typewriter = new Typewriter(flowInput, {
+  loop: false,
+  delay: 75
+});
+
 // cssanimation.rocks
 var scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};
 
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
+const toggleVisibility = document.querySelector('.flow-list');
+
+console.log("Toggle: ", toggleVisibility);
 
 function loop() {
 
     elementsToShow.forEach(function (element) {
       if (isElementInViewport(element)) {
         element.classList.add('is-visible');
+
+        if (!animationStarted) {
+          toggleVisibility.classList.add('is-visible');
+          typewriter
+            .pauseFor(4000)
+            .typeString('Hoi,  ')
+            .pauseFor(3250)
+            .typeString('<div class="flow-tag flow-name"><span>Naam</span></div>')
+            .typeString('.')
+            .start();
+          animationStarted = true;
+        }
       } else {
+        console.log("Toggle: ", toggleVisibility);
         element.classList.remove('is-visible');
+
+        if (animationStarted) {
+          typewriter.deleteAll(15);
+
+          toggleVisibility.classList.remove('is-visible');
+        animationStarted = false;
+        }
       }
     });
   
@@ -37,19 +70,3 @@ function isElementInViewport(el) {
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
     );
   }
-
-// Typewriter
-var flowInput = document.getElementById('flow-input');
-
-var typewriter = new Typewriter(flowInput, {
-  loop: false,
-  delay: 75,
-});
-
-typewriter
-  .pauseFor(4000)
-  .typeString('Hoi,  ')
-  .pauseFor(3250)
-  .typeString('<div class="flow-tag flow-name"><span>Naam</span></div>')
-  .typeString('.')
-  .start();
